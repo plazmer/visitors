31e540ba968521d09834dfb4236d3dc7bb6b4e22
PrAW <praw2003@gmail.com>
2020-10-30 18:14:55 +0500

    bottle-pymysql replacement

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 from bottle import route, run, template, request, error,  default_app
+import bottle_pymysql
 import datetime
 
 import pymysql.cursors
 import config
 
 app = default_app()
-conn = pymysql.connect(host=config.DB_HOST,
-                             user=config.DB_USER,
-                             password=config.DB_PASS,
-                             db=config.DB_DATABASE,
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-
+plugin = bottle_pymysql.Plugin(dbhost=config.DB_HOST,
+                             dbuser=config.DB_USER, 
+                             dbpass=config.DB_PASS, 
+                             dbname=config.DB_DATABASE,
+                             charset='utf8mb4')
+app.install(plugin)
 
 @route('/list')
-def visitors_list():
-    c = conn.cursor()
-    c.execute("SELECT id, groups, fio, datetime FROM visitors ORDER BY id, groups")
-    result = c.fetchall()
-    c.close()
+def visitors_list(pymydb):    
+    pymydb.execute("SELECT id, groups, fio, datetime FROM visitors ORDER BY id, groups")
+    result = pymydb.fetchall()
 
     output = template('make_table', rows=result)
     return output
@@ -30,18 +28,15 @@ def index():
 
 
 @route('/new', method='POST')
-def addnew():
+def addnew(pymydb):
     group = request.POST.group.strip()
     fio = request.POST.fio.strip()
 
     if not group or not fio:
-        return '<a href="/new">ты что-то забыл указать, вернись</a>'
+        return '<a href="/">ты что-то забыл указать, вернись</a>'
 
-    c = conn.cursor()
-    c.execute("INSERT INTO visitors (groups, fio) VALUES (%s,%s)", (group, fio))
-    new_id = c.lastrowid
-    conn.commit()
-    c.close()
+    pymydb.execute("INSERT INTO visitors (groups, fio) VALUES (%s,%s)", (group, fio))
+    new_id = pymydb.lastrowid
 
     return '<p>Я запомню тебя, %s, за номером %s</p>' % (fio, new_id)
 
